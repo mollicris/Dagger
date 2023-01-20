a6cdd8008a1a0c718e5a872501ae85cca964c84b
crmollinedo <cristianm184@hotmail.com>
2023-01-19 23:48:00 -0400

    Using the interface for DI

--- app/src/main/java/com/anushka/didemo/Battery.kt
+++ app/src/main/java/com/anushka/didemo/Battery.kt
@@ -3,12 +3,14 @@ package com.anushka.didemo
 import android.util.Log
 import javax.inject.Inject
 
-class Battery @Inject constructor(){
-    init {
+interface Battery {
+    fun getPower()
+  /*  init {
         Log.i("MYTAG","Battery Constructed")
-    }
+    }*/
 
-    fun getPower(){
+
+    /*{
         Log.i("MYTAG","Battery power is available")
-    }
+    }*/
 }
---
+++ app/src/main/java/com/anushka/didemo/NCBBatteryModule.kt
@@ -0,0 +1,18 @@
+package com.anushka.didemo
+
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+
+@Module
+abstract class NCBatteryModule {
+    @Binds
+    abstract fun bindsNCBattery(nickelCadmiumBattery: NickelCadmiumBattery):Battery
+}
+/*@Module
+class NCBBatteryModule {
+    @Provides
+    fun providesNCBattery(nickelCadmiumBattery: NickelCadmiumBattery) : Battery{
+        return nickelCadmiumBattery
+    }
+}*/
